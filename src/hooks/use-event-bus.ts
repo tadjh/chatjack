5ab8f862c210cef2fb2415e773d1870e5e7a163e
tadjh <hello@tadjh.com>
2025-03-02 10:55:13 -0800

    Clean up

--- src/hooks/use-event-bus.ts
+++
@@ -1,7 +0,0 @@
-import { EventBus, eventBus } from "@/lib/event-bus";
-import { useRef } from "react";
-
-export function useEventBus() {
-  const eventBusRef = useRef<EventBus>(eventBus);
-  return eventBusRef.current;
-}
