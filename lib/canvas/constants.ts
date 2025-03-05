99d29e3d7715af72f7d5e59584b3aef1d34cfced
tadjh <hello@tadjh.com>
2025-03-04 19:24:30 -0800

    Add subsets

--- lib/canvas/constants.ts
+++ lib/canvas/constants.ts
@@ -6,12 +6,14 @@ const jacquard = Jacquard_24({
   variable: "--font-jacquard",
   weight: ["400"],
   display: "block",
+  subsets: ["latin"],
 });
 
 const pressStart = Press_Start_2P({
   variable: "--font-press-start",
   weight: ["400"],
   display: "block",
+  subsets: ["latin"],
 });
 // const isDevelopment = process.env.NODE_ENV === "development";
 
